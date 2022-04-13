00d5f1277d447ae62c9791d3c2d0c37a24d69346
LuisDiego10 <guisogarcia1010@estudiantec.cr>
2022-04-13 11:59:31 -0600

    Create method v1.0

--- CRUD.py
+++ CRUD.py
@@ -1,10 +1,27 @@
+estudiantes = {}
 loop = True
+
+def Create():
+    carne=input("Ingrese el número de carnet del estudiante")
+    add_courses=True
+    cursos={}
+    course=input("Ingrese el nombre del curso")
+    cursos[course]=0
+    while add_courses==True:
+        answer=input("¿Desea agregar mas cursos?\n")
+        if answer.lower() =="si" or answer.lower()=="sí" or answer.lower()=="yes":
+            course=input("Ingrese el nombre del curso")
+            cursos[course]=0
+        else:
+            print(cursos)
+            add_courses=False
+
 while loop:  
     print("\nMenu: \n1. Create \n2. Read \n3. Update \n4. Delete \n5. Exit ")
     option=int(input("Por favor ingrese deseada del menú anterior:"))
     match option:
         case 1:
-            print("\n Create")
+            Create()
         case 2:
            print("\n Read")
         case 3:        
@@ -15,3 +32,7 @@ while loop:
             print("\n Gracias por usar el programa")
             loop=False
             break;
+
+
+
+    
